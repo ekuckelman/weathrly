7f15ce19ab3a14e1de71328653d12c53b3bb7c5f
Melissa Wagasky <melissawagasky@gmail.com>
2018-01-07 11:31:47 -0700

    Add file change

--- test/App.test.js
+++ test/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import App from '../lib/App.js'
-// import mockData from '../__mocks__/testMock.js'
 
 describe('App', () => {
 
