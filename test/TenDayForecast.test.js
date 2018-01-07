2fae1260cdcfb0afcc7c7be0481ebffc202d1aaf
Nora Gully <nora.gully@gmail.com>
2018-01-07 16:20:03 -0700

    Complete testing on Current, TenDayForecast, HourlyForecast
    by passing mock object or array to each of these files in the place of their this.props

--- test/TenDayForecast.test.js
+++ test/TenDayForecast.test.js
@@ -2,15 +2,41 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import TenDayForecast from '../lib/TenDayForecast.js'
 
-describe.skip('TenDayForecast', () => {
+const tenDay = [
+  {tenDayDay: 'Tuesday', tenImg: 'http://icons.wxug.com/i/c/k/snow.gif', tenHigh: 24, tenLow: 10},
+  {tenDayDay: 'Wednesday', tenImg: 'http://icons.wxug.com/i/c/k/snow.gif', tenHigh: 24, tenLow: 10},
+  {tenDayDay: 'Thursday', tenImg: 'http://icons.wxug.com/i/c/k/snow.gif', tenHigh: 24, tenLow: 10},
+  {tenDayDay: 'Friday', tenImg: 'http://icons.wxug.com/i/c/k/snow.gif', tenHigh: 24, tenLow: 10},
+  {tenDayDay: 'Saturday', tenImg: 'http://icons.wxug.com/i/c/k/snow.gif', tenHigh: 24, tenLow: 10},
+  {tenDayDay: 'Sunday', tenImg: 'http://icons.wxug.com/i/c/k/snow.gif', tenHigh: 24, tenLow: 10},
+  {tenDayDay: 'Monday', tenImg: 'http://icons.wxug.com/i/c/k/snow.gif', tenHigh: 24, tenLow: 10},
+  {tenDayDay: 'Tuesday', tenImg: 'http://icons.wxug.com/i/c/k/snow.gif', tenHigh: 24, tenLow: 10},
+  {tenDayDay: 'Wednesday', tenImg: 'http://icons.wxug.com/i/c/k/snow.gif', tenHigh: 24, tenLow: 10},
+  {tenDayDay: 'Thursday', tenImg: 'http://icons.wxug.com/i/c/k/snow.gif', tenHigh: 24, tenLow: 10}
+]
+
+describe('TenDayForecast', () => {
 
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<TenDayForecast />)
+    wrapper = shallow(<TenDayForecast tenDayData={tenDay}/>)
   })
 
   it('should exist', () => {
     expect(wrapper).toBeDefined();
   })
+
+
+  it('should display 10 cards', () => {
+    expect(wrapper.find('Card').length).toEqual(10)
+  })
+
+  it('should display ', () => {
+    wrapper = mount(<TenDayForecast tenDayData={tenDay} />)
+    expect(wrapper.find('h3').first().text()).toEqual('Tuesday')
+    expect(wrapper.find('img').first().prop('src')).toEqual('http://icons.wxug.com/i/c/k/snow.gif')
+    expect(wrapper.find('p').first().text()).toEqual('24')
+  })
+
 })
