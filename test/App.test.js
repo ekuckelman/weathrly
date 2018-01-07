e1c0111bfc031340ef823ce5f58ee79479312400
Nora Gully <nora.gully@gmail.com>
2018-01-07 11:53:08 -0700

    Add global.localStorage object to App.test.js to simulate localStorage

--- test/App.test.js
+++ test/App.test.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import App from '../lib/App.js'
+import mockData from '../lib/mockData.js'
+
+global.localStorage = {
+  getItem(keyword) {
+    if (!global.localStorage[keyword]) {
+      return null;
+    }
+    return JSON.stringify(global.localStorage[keyword]);
+  },
+  setItem(keyword, value) {
+    global.localStorage[keyword] = value;
+  }
+};
+
 
 describe('App', () => {
 
@@ -13,23 +27,27 @@ describe('App', () => {
   it.only('should exist', () => {
     expect(wrapper).toBeDefined();
   })
+
+
 })
 
- // it('should render the Controls and TriviaList components', () => {
- //    expect(wrapper.find('h1').length).toEqual(1)
- //    expect(wrapper.find('Controls').length).toEqual(1)
- //    expect(wrapper.find('TriviaList').length).toEqual(1)
- //  })
+//  it('should render the Controls and TriviaList components', () => {
+//     expect(wrapper.find('h1').length).toEqual(1)
+//     expect(wrapper.find('Controls').length).toEqual(1)
+//     expect(wrapper.find('TriviaList').length).toEqual(1)
+//   })
 
- // it('should have a default of an empty state array triviaList', () => {
- //  expect(wrapper.state().triviaList).toEqual([])
- // })
+//  it('should have a default of an empty state array triviaList', () => {
+//   expect(wrapper.state().triviaList).toEqual([])
+//  })
 
- // it('should update state', () => {
- //  expect(wrapper.state().triviaList).toEqual([]);
+//  it('should update state', () => {
+//   expect(wrapper.state().triviaList).toEqual([]);
 
- //  wrapper.setState({ triviaList: mockData.results })
+//   wrapper.setState({ triviaList: mockData })
 
- //  expect(wrapper.state().triviaList).toEqual(mockData.results);
- //  expect(wrapper.state().triviaList.length).toEqual(10);
- // })
+//   expect(wrapper.state().triviaList).toEqual(mockData.results);
+//   expect(wrapper.state().triviaList.length).toEqual(10);
+//  })
+
+// })
